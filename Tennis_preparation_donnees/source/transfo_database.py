4b75eedbaa66a3fa04b7b95b55bb999e3fe0225c
DarrkhI <77415727+DarrkhI@users.noreply.github.com>
2021-02-07 10:41:22 +0100

    Update transfo_database.py

--- Tennis_preparation_donnees/source/transfo_database.py
+++ Tennis_preparation_donnees/source/transfo_database.py
@@ -1,125 +1,203 @@
 # -*- coding: utf-8 -*-
 """
-Transformation de database
-
+Database transformation
+author: @Darrkhl
 """
 #Libraries
 
 import random
 import csv 
+import os
 
-#pour tester des morceaux de code et que le vecteur avec une donnée aleatoire ne change pas 
-#à paritr du moment où vous l'avez lacé la premiere fois
 #random.seed(10)
 
-#FONCTIONS
+#FUNCTIONS
+         
+#we take all the .CSV files of a specific folder, for each of them we only keep the rows we need
+def get_all_csv(directory, delim):
+    db = []
+    for filename in os.listdir(directory):        
+        path = (directory + '/' + filename)
+        if path.endswith(".csv"):
+            file = []
+            with open(path, 'r', encoding = 'utf-8-sig') as csv_file :
+                reader_csv = csv.reader(csv_file, delimiter=delim)
+                for line in reader_csv :
+                    file.append(line)                           
+                db.append(file)    
+        continue                   
+    return db
 
-#on ouvre / lit un fichier csv encodé en utf-8
-def lecture_csv(fichier):
-    lignes = []
-    with open(fichier, 'r', encoding = 'utf-8') as fichier_csv :
-      lecteur_csv = csv.reader(fichier_csv, delimiter=";")
-      for ligne in lecteur_csv :
-          lignes.append(ligne)
-    return lignes
+#we only work on the grand slam matches
+def select_series(db, column = "Series", series = "Grand Slam"):
+    final_db = []   
+    for file in db:
+        temp_db = []
+        if len(file) > 0:                     
+            temp_db.append(file[0])
+            for line in file:
+                if line[file[0].index(column)] == series:
+                    temp_db.append(line)   
+                
+        final_db.append(temp_db)
+    return final_db
 
-#on concatene toutes les matrices qui vont servir à l'apprentissage
-def concatenation_GS(liste_bdd):
-    base_complete = []
-    for bdd in liste_bdd:
-        for ind in range(len(bdd)):
-            if bdd_2010[ind][2] == "Grand Slam":
-                base_complete.append(bdd[ind])
-    return base_complete
-
-#on prend les infos qui nous interessent par joueur
-def joueur_infos(ind_nom_joueur,ind2,ind3,ind4,ind5, bdd):
-    infos = []
-    for ligne in bdd:
-            infos.append([ligne[ind_nom_joueur],ligne[ind2], ligne[ind3], ligne[ind4], ligne[ind5]])    
-    return infos
-
-#transformation d'une liste de listes en un fichier csv en utf-8
-def ecriture(nom_fichier,bdd):
-    with open(nom_fichier, 'w', newline='', encoding = 'utf-8') as fichier_csv:
-        contenu = csv.writer(fichier_csv, delimiter=';')
-        if type(bdd) is list :
-            for ligne in bdd:
-                if type(ligne) is list and len(ligne)>1 :
-                    contenu.writerow(ligne)
-                    
-#concatenation des vecteurs et matrices
-def modif_bdd(winner, loser, bdd_vierge):   
-    for indice in range(len(winner)):
-        ligne_bdd = []
-        ligne_bdd.append(tournois[indice])
-        ligne_bdd.append(surface[indice])
-        ligne_bdd.append(Date[indice])
-        ligne_bdd.append(Round[indice])
-        if winner[indice][1] == "J1":
-            for j in range(len(winner[indice][0])):
-                ligne_bdd.append(winner[indice][0][j])
-                ligne_bdd.append(loser[indice][0][j])                                
-        elif winner[indice][1] == "J2":
-            for j in range(len(winner[indice][0])):
-                ligne_bdd.append(loser[indice][0][j])
-                ligne_bdd.append(winner[indice][0][j])
-            
-        ligne_bdd.append(winner[indice][1])
-        
-        bdd_vierge.append(ligne_bdd)
-    return bdd_vierge                    
- 
-#CORPS DU PROGRAMME
-
-#travail sur les base de données
-bdd_2010 = lecture_csv("../csv/2010.csv")
-bdd_2011 = lecture_csv("../csv/2011.csv")
-bdd_2012 = lecture_csv("../csv/2012.csv")
-bdd_2013 = lecture_csv("../csv/2013.csv")
-bdd_2014 = lecture_csv("../csv/2014.csv")
-bdd_2015 = lecture_csv("../csv/2015.csv")
-bdd_2016 = lecture_csv("../csv/2016.csv")
-bdd_2017 = lecture_csv("../csv/2017.csv")
-bdd_2018 = lecture_csv("../csv/2018.csv")
-
-bdd = [bdd_2010, bdd_2011, bdd_2012, bdd_2013, bdd_2014, bdd_2015]
-
-base_brute = concatenation_GS(bdd)
-
-                ###############
-
-#matrices avec les infos des maths par joueur gagnant/perdant
-winner = joueur_infos(5,7,9,11, 13, base_brute)
-loser = joueur_infos(6,8,10,12,14, base_brute)
-
-#randomisation de l'attribution du J1/J2
-random_j1j2 = [random.choice(("J1","J2")) for i in range(len(winner))]
-
-#attribution J1/J2 par outcome
-winner_num = [[x,y] for x,y in zip(winner,random_j1j2)]
-loser_num = []
-for indice in range(len(winner_num)):
-    if winner_num[indice][1] == "J2":
-        loser_num.append([loser[indice],"J1"])
-    elif winner_num[indice][1] == "J1":
-        loser_num.append([loser[indice],"J2"])
+#we keep only the columns (entered in the desired order) we are interested in
+def selected_columns(db, *columns):
+     final_db = []         
+     #header for our final db     
+     col = [c for c in columns]
+     final_db.append(col)
+     
+     for file in db:
+         ind = [file[0].index(el) for el in col]         
+         for line in file[1:]:
+             new_line = [line[i] for i in ind]              
+             final_db.append(new_line)        
     
-                   ###############
+     return final_db
+
+#(if relevant) we only consider a specific round of play
+def select_round(db, Round = "1st Round"):    
+    ind = db[0].index("Round")      
+    final_db = [line for line in db[1:] if line[ind] == Round]    
+    final_db.insert(0, db[0])
+    return final_db
+ 
+#(if relevant) we only consider player ranked under a set number
+def select_players_ranks(db, max_rank = 11):   
+    ind = [db[0].index("WRank"), db[0].index("LRank")]
+    final_db = []
+    for line in db[1:]:
+        try :        
+            if int(line[ind[0]]) < max_rank :
+                pass
+            else : 
+                final_db.append(line)
+        except ValueError:
+            final_db.append(line)
+    final_db.insert(0, db[0])
+    return final_db
+     
+#randomisation of the attribution : player 1/2 
+def randomisation_player(db, win = "Winner", name1 = "P1", name2 = "P2"):  
+    random_p1p2 = [random.choice((name1,name2)) for i in range(len(db[1:]))]         
+    winner = [db[1:][i][db[0].index(win)] for i in range(len(db[1:]))]              
+    return [[x,y] for x,y in zip(winner,random_p1p2)]   
+
+#first step : transform the structure of the db, we're left with only Player 1 or 2 
+#no reference to winner or loser 
+def rename_type(db, list_winner, *columns_winner):
+    final_db = []        
+    ind_col_w = [db[0].index(c) for c in columns_winner]
+    ind_col_l = [i+1 for i in ind_col_w ]
+    
+    for i_win in range(len(list_winner)):        
+        if list_winner[i_win][1] == "P1":
+            play1 = [db[1:][i_win][index_win] for index_win in ind_col_w]                        
+            play2 = [db[1:][i_win][index_los] for index_los in ind_col_l]     
+              
+            temp = []
+            for el in range(len(ind_col_w)) :
+                temp.append(play1[el])
+                temp.append(play2[el])   
+            temp.append("P1")
+            
+        elif list_winner[i_win][1] == "P2":
+            play1 = [db[1:][i_win][index_los] for index_los in ind_col_l] 
+            play2 = [db[1:][i_win][index_win] for index_win in ind_col_w]                                                              
+            temp = []
+            for el in range(len(ind_col_l)) :
+                temp.append(play1[el])
+                temp.append(play2[el])     
+            temp.append("P2")
+                
+        final_db.append(temp)        
+    return final_db
+
+#merge player infos with match info with 
+def concat(header, db_total, player_info, general_col = 4):
+    final_db = [header]
+    
+    for ind in range(len(db_total[1:])):
+        temp = []
+        for j in range(general_col):
+            temp.append(db_total[1:][ind][j])
+        for k in range(len(player_info[0])):
+            temp.append(player_info[ind][k])
+            
+        final_db.append(temp)
+    return final_db
+
+#take the final database and switch which player is player 1/player 2 (in order to measure where our model depends on player1/2)
+def switchp1_p2(db_p1p2, general_col = 4, type1 = "P1", type2 = "P2"):
+    final_db = [db_p1p2[0]]
+    for ind in range(len(db_p1p2[1:])):
+        temp = []
+        for j in range(general_col):
+            temp.append(db_p1p2[1:][ind][j])
+        for k in range(general_col+1,len(db_p1p2[0]), 2):
+            temp.append(db_p1p2[1:][ind][k])
+            temp.append(db_p1p2[1:][ind][k-1])
+        if db_p1p2[1:][ind][-1] == type1:
+            temp.append(type2)
+        else :
+            temp.append(type1)
+        
+        final_db.append(temp)
+    return final_db
+
+      
+#transform a list of lists into a csv file utf-8 encoded 
+def list_to_csv(file_name,db, delim):
+    with open(file_name, 'w', newline='', encoding = 'utf-8-sig') as csv_file:
+        content = csv.writer(csv_file, delimiter=delim)
+        if type(db) is list :
+            for line in db:
+                if type(line) is list and len(line)>1 :
+                    content.writerow(line)
                     
-#creation nouvelle bdd
-bdd_entete = [["Tournament", "Surface", "Date", "Round", "J1", "J2", "J1Rank", "J2Rank", "J1Pts", "J2Pts", "B365J1", "B365J2", "PSJ1", "PSJ2","res"]]
+##############################################################################                                 
+ 
+#CORE PROGRAM 
 
-#Creation vecteurs pas relatifs aux joueurs
-tournois = [base_brute[i][0] for i in range(len(base_brute))]
-surface = [base_brute[i][3] for i in range(len(base_brute))]
-Date = [base_brute[i][1] for i in range(len(base_brute))]
-Round = ["1st Round" for x in range(len(winner_num))] 
-   
+#First phase on raw data
 
-#dernieres manip : concatenation et transformation de la base finale en fichier csv   
-base_finale = modif_bdd(winner_num, loser_num, bdd_entete)
-#tennis_ = ecriture("tennis_2010_2015.csv", base_finale)   
+all_csv = get_all_csv(directory = "../csv", delim = ";")
+grand_slams = select_series(all_csv, column = "Series", series = "Grand Slam")
+
+#for example 1 
+#gs_reduced = selected_columns(grand_slams, "Tournament", "Surface", "Date", "Round", "Winner", "Loser", "WRank", "LRank", "WPts", "LPts", "B365W", "B365L", "PSW", "PSL")
+
+#for example 2
+gs_reduced2 = selected_columns(grand_slams, "Tournament", "Surface", "Date", "Round", "Winner", "Loser","W1", "L1", "W2", "L2", "W3", "L3",  "WRank", "LRank", "WPts", "LPts", "B365W", "B365L", "PSW", "PSL")
+
+            #######################
+  
+#Final phase on processed data 
+
+#Example 1 with just basic data
+
+#gs_1stRound = select_round(gs_reduced, Round = "1st Round")
+#gs_1stRound_rank11plus = select_players_ranks(gs_1stRound, max_rank = 11)
+#winner = randomisation_player(gs_1stRound_rank11plus)
+#player12_infos = rename_type(gs_1stRound_rank11plus, winner, 'Winner', 'WRank', 'WPts', "B365W","PSW")
+#db_header = ["Tournament", "Surface", "Date", "Round", "J1", "J2", "J1Rank", "J2Rank", "J1Pts", "J2Pts", "B365J1", "B365J2", "PSJ1", "PSJ2","res"]
+#final_db = concat(db_header, gs_1stRound_rank11plus, player12_infos, general_col= 4)
+#file_final_db = list_to_csv("final_db_2010-2018.csv", final_db)
+#final_db_p1p2switch = switchp1_p2(final_db, general_col = 4, type1 = "P1", type2 = "P2")
+#file_final_db_switch = list_to_csv("final_db_2010-2018_switch.csv", final_db_p1p2switch)
 
 
+#Example with basic info +  columns of number of games won per sets for winner /loser 
+
+gs_1stRound2 = select_round(gs_reduced2, Round = "1st Round")
+gs_1stRound_rank11plus2 = select_players_ranks(gs_1stRound2, max_rank = 11)
+winner2 = randomisation_player(gs_1stRound_rank11plus2)
+player12_infos2 = rename_type(gs_1stRound_rank11plus2, winner2, 'Winner', "W1", "W2", "W3",'WRank', 'WPts', "B365W","PSW")
+db_header2 = ["Tournament", "Surface", "Date", "Round", "J1", "J2", "J1set1", "J2set1", "J1set2", "J2set2", "J1set3", "J2set3", "J1Rank", "J2Rank", "J1Pts", "J2Pts", "B365J1", "B365J2", "PSJ1", "PSJ2","res"]
+final_db2 = concat(db_header2, gs_1stRound_rank11plus2, player12_infos2, general_col= 4)
+#file_final_db2 = list_to_csv("final_db2_2010-2018.csv", final_db2)
+final_db_p1p2switch2 = switchp1_p2(final_db2, general_col = 4, type1 = "P1", type2 = "P2")
+#file_final_db_switch2 = list_to_csv("final_db2_2010-2018_switch.csv", final_db_p1p2switch2)
 
